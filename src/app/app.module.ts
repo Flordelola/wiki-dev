ad39dbd03d9dc65e27d6f349e45f8a31c1251b12
Flordelola <mhelenaamador@gmail.com>
2017-07-13 16:40:37 +0200

    nuevas carpetas, modificaciones varias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { ROUTING } from './app.routing';
+import { HeaderComponent } from './header.component';
+import { BuscadorComponent } from './buscador/buscador.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ListArticlesComponent } from './list-articles/list-articles.component';
+import { BuscadorPipe } from './buscador.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BuscadorComponent,
+    ArticlesComponent,
+    ListArticlesComponent,
+    BuscadorPipe
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ROUTING,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+
+const APP_ROUTES: Routes = [
+   
+];
+
+export const ROUTING = RouterModule.forRoot(APP_ROUTES);
